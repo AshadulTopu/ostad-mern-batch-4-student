051667ddafc99452bad5fece5d7ff05a9be264ad
AshadulTopu <ashadulislam2003@gmail.com>
2023-10-01 21:28:30 +0600

    everything updated

---
+++ Module-7/live-test/index.js
@@ -0,0 +1,56 @@
+
+// // What is Server-Side Rendering?
+// Server-Side Rendering (SSR) is a web development technique where website render in server, it used to improve the performance, security and search engine optimization of web applications.
+
+// // What are the Benefits of Server-Side Rendering?
+// Better SEO 
+// Faster initial lading
+// improved security 
+// better performance on low power devices 
+// Social Media Sharing
+
+// // What Is Client-Side Rendering, And How Does It Work?
+// Client-Side Rendering (CSR) is a web development technique where web pages are rendered in the user's web browser using JavaScript and HTML.
+
+
+// // Explain HTTP client in detail.
+// HTTP clients means where to request Come. 
+// HTTP means (Hypertext Transfer Protocol). It is a request response model for client side to server side. 
+// it's has sevarel part like, URL, Request method, headers, request body.respons. 
+
+
+// //Describe the best practices of JSON.
+// start with {} 
+// cover all key and value with ""
+// using data type
+// replace all bad character 
+
+
+// //Why are we using JSON Web Token in the project?
+// for web application security we use JWT (JSON Web Tokens)
+
+// //Why do we create an Undefined Route in the project?
+// app.use('*' ()=>{})
+
+// // What is an environment variable and why do we use it?
+
+// An environment variable is a dynamic, key-value pair that is part of the environment in which a process runs.  Environment variables are typically set outside of an application's code and are available to the application at runtime.
+// These variables are used to store configuration settings, system information, and other data that can be accessed and manipulated 
+
+
+// //Briefly describe, how you would create the project structure of ExpressJs.
+
+// first create jason file using npm init --y
+// then install express js 
+// after install express js create those directory and file 
+// index.js is a main file 
+// app.js second main file 
+
+// then create src folder 
+// inside src folder create controller folder for controller file, create helper folder for helper file, create middleware folder for middleware file, create model folder for model file, create routes folder for route file 
+
+
+
+// // Why is Encryption Decryption used?
+
+// Encryption and decryption are used to protect sensitive information and ensure the confidentiality and security of data during transmission and storage. 
